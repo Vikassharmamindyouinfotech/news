e3311323951e2ff9d12193d671cb2335c053dce7
Vikassharmamindyouinfotech <151121292+Vikassharmamindyouinfotech@users.noreply.github.com>
2024-02-02 16:12:19 +0000

    signup page done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./pages/login/login.component";
+import { SignupComponent } from "./pages/signup/signup.component";
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, LoginComponent]
+    imports: [RouterOutlet, LoginComponent, SignupComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'news';
