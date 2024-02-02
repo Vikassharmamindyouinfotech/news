48cc2e63f91d55b01913c3b3356007a633dfb409
vikassharmanaini <vikassharmanaini@gmail.com>
2024-02-02 22:18:50 +0530

    Update to Angular routing, header component, and styles. (generated by blackbox.ai )

--- src/app/app.config.server.ts
+++ src/app/app.config.server.ts
@@ -1,14 +1,12 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
-import {  MatIcon } from "@angular/material/icon";
+// import {  MatIcon } from "@angular/material/icon";
 const serverConfig: ApplicationConfig = {
   providers: [
     provideServerRendering()
   ]
-  dependency:[
-
-  ]
+ 
 };
 
 export const config = mergeApplicationConfig(appConfig, serverConfig);
